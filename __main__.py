d6e93a63a2492a2283d9f0095bca63acc46c60c9
Elijah Oliphant <elephant2go@gmail.com>
2022-03-02 17:36:58 -0700

    coment

--- __main__.py
+++ __main__.py
@@ -21,7 +21,7 @@ FONT_SIZE = 15
 COLS = 60
 ROWS = 40
 CAPTION = "Greed"
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/messages.txt"
+#DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
 
@@ -49,15 +49,22 @@ def main():
     robot.set_color(WHITE)
     robot.set_position(position)
     cast.add_actor("robots", robot)
+
     
+
+
+    #Actor.get_velocity(10)
+    #cast.set_position(position)
+
     # create the artifacts
-    with open(DATA_PATH) as file:
-        data = file.read()
-        messages = data.splitlines()
+    #with open(DATA_PATH) as file:
+    #    data = file.read()
+    #    messages = data.splitlines()
 
     for n in range(DEFAULT_ARTIFACTS):
-        text = chr(random.randint(33, 126))
-        message = messages[n]
+        starsim = "*"
+        #text = chr(random.randint(33, 126))
+        #message = messages[n]
 
         x = random.randint(1, COLS - 1)
         y = random.randint(1, ROWS - 1)
@@ -67,15 +74,43 @@ def main():
         r = random.randint(0, 255)
         g = random.randint(0, 255)
         b = random.randint(0, 255)
-        color = Color(r, g, b)
+        color = Color(255, 255, b)
         
-        artifact = Artifact()
-        artifact.set_text(text)
-        artifact.set_font_size(FONT_SIZE)
-        artifact.set_color(color)
-        artifact.set_position(position)
-        artifact.set_message(message)
-        cast.add_actor("artifacts", artifact)
+        star = Artifact()
+        star.set_text(starsim)
+        star.set_font_size(FONT_SIZE)
+        star.set_color(color)
+        star.set_position(position)
+        star.set_message("points:")
+        cast.add_actor("artifacts", star)
+
+
+    for n in range(DEFAULT_ARTIFACTS):
+        rocksim = "[]"
+        #text = chr(random.randint(33, 126))
+        #message = messages[n]
+
+        x = random.randint(1, COLS - 1)
+        y = random.randint(1, ROWS - 1)
+        position = Point(x, y)
+        position = position.scale(CELL_SIZE)
+
+        r = random.randint(0, 100)
+        g = random.randint(0, 100)
+        b = random.randint(50, 200)
+        color = Color(b, b, b)
+        
+        rock = Artifact()
+        rock.set_text(rocksim)
+        rock.set_font_size(FONT_SIZE)
+        rock.set_color(color)
+        rock.set_position(position)
+        rock.set_message("points:")
+        cast.add_actor("artifacts", rock)
+
+        #Actor.get_velocity(10)
+        #rock.set_position(Point(x, y))
+        
     
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
