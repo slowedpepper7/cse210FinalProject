0dd312bd60e8435e1880d31ecab2c3c8db4d6623
slowedpepper7 <97586351+slowedpepper7@users.noreply.github.com>
2022-04-01 21:51:57 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,17 +1,17 @@
 import os
 import random
 
-from game.casting.actor import Actor
-from game.casting.artifact import Artifact
-from game.casting.cast import Cast
+from actor import Actor
+from artifact import Artifact
+from cast import Cast
 
-from game.directing.director import Director
+from director import Director
 
-from game.services.keyboard_service import KeyboardService
-from game.services.video_service import VideoService
+from keyboard_service import KeyboardService
+from video_service import VideoService
 
-from game.shared.color import Color
-from game.shared.point import Point
+from color import Color
+frompoint import Point
 
 
 FRAME_RATE = 12
